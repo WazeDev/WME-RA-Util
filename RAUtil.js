48c59b34797970d56be5a2e3d5c316ec9db006a0
justins83 <justins83.waze@gmail.com>
2022-08-12 11:13:49 -0400

    Update RAUtil.js

--- RAUtil.js
+++ RAUtil.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME RA Util
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2022.08.12.01
+// @version      2022.08.12.02
 // @description  Providing basic utility for RA adjustment without the need to delete & recreate
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
