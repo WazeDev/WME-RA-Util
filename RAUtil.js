03c81b2f134915fc76a9b06e29a85927f3e41a9f
Lachlan Mackenzie <36923157+lacmac@users.noreply.github.com>
2023-07-17 17:27:42 +0800

    Fixed shift right functionality

--- RAUtil.js
+++ RAUtil.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME RA Util
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2023.07.17.01
+// @version      2023.07.14.02
 // @description  Providing basic utility for RA adjustment without the need to delete & recreate
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -738,7 +738,7 @@ normal RA color:#4cc600
 
         //if(!pendingChanges){
         var segObj = WazeWrap.getSelectedFeatures()[0];
-        var convertedCoords = WazeWrap.Geometry.ConvertTo4326(segObj.model.geometry.components[0].x, segObj.model.geometry.components[0].y);
+        var convertedCoords = WazeWrap.Geometry.ConvertTo4326(segObj.geometry.components[0].x, segObj.geometry.components[0].y);
         var gpsOffsetAmount = WazeWrap.Geometry.CalculateLongOffsetGPS($('#shiftAmount').val(), convertedCoords.lon, convertedCoords.lat);
         ShiftSegmentsNodesLong(segObj, gpsOffsetAmount);
         //}
